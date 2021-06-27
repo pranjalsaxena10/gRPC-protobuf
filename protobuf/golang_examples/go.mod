6452a118d796cea2be786bb5490741260badf506
pranjalsaxena10 <pranjalmohansaxena@gmail.com>
2021-06-27 19:08:53 +0530

    Changes for reading and writing to file incase of protobuf

--- protobuf/golang_examples/go.mod
+++ protobuf/golang_examples/go.mod
@@ -2,4 +2,7 @@ module github.com/pranjalsaxena10/gRPC-protobuf/protobuf/golang_examples
 
 go 1.16
 
-require google.golang.org/protobuf v1.26.0-rc.1
+require (
+	github.com/spf13/pflag v1.0.5 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1
+)
--- protobuf/golang_examples/main.go
+++ protobuf/golang_examples/main.go
@@ -1,21 +1,71 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"github.com/spf13/pflag"
+
 	"github.com/pranjalsaxena10/gRPC-protobuf/protobuf/golang_examples/generated"
+	"google.golang.org/protobuf/proto"
 )
 
+
 func main() {
-	createSimpleMessageStruct()
+	var fileName string
+	pflag.StringVarP(&fileName, "fileName", "", "file.bin", "Name of file")
+	pflag.Parse()
+	SimpleMessageReadWriteFromFile(fileName)
+
 }
 
-func createSimpleMessageStruct() {
+func SimpleMessageReadWriteFromFile(fileName string) {
+	simpleMessage := createSimpleMessageStruct()
+	writeToFile(fileName, simpleMessage)
+	readFromFile(fileName, simpleMessage)
+}
+
+func readFromFile(fileName string, simpleMessage *simplepb.SimpleMessage) {
+	log.Print("readFromFile started...")
+	
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal("Error occurred while reading from ", fileName, "err: ", err)
+	}
+
+	err = proto.Unmarshal(data, simpleMessage)
+	if err != nil {
+		log.Fatal("Error occurred while unmarshalling ", simpleMessage, "err: ", err)
+	}
+
+	log.Printf("Reading from file is successful. Proto: %+v", simpleMessage)
+}
+
+func writeToFile(fileName string, simpleMessage *simplepb.SimpleMessage) {
+	log.Print("writeToFile started...")
+
+	protoData, err := proto.Marshal(simpleMessage)
+
+	if err != nil {
+		log.Fatal("Error occurred while marshalling ", simpleMessage, "err: ", err)
+	}
+
+	err = ioutil.WriteFile(fileName, protoData, 0644)
+
+	if err != nil {
+		log.Fatal("Error occurred while writing to file: ", fileName, "err : ", err)
+	}
+
+	log.Print("Writing to file is successful")
+}
+
+
+
+func createSimpleMessageStruct() *simplepb.SimpleMessage{
 	value := simplepb.SimpleMessage{
 		Id:          456,
 		IsPresent:   true,
 		MessageName: "message from proto",
 		List:        []int32{5, 7, 8},
 	}
-	fmt.Println(value)
-
+	return &value
 }
