0218add58b86984bcf5b818dd5c164a202ca9ef9
pranjalsaxena10 <pranjalmohansaxena@gmail.com>
2021-06-27 15:02:10 +0530

    Changes for using generated pb go struct

--- protobuf/golang_examples/main.go
+++ protobuf/golang_examples/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pranjalsaxena10/gRPC-protobuf/protobuf/golang_examples/generated"
+)
 
 func main() {
-	fmt.Println("Protobuf example")
+	createSimpleMessageStruct()
+}
+
+func createSimpleMessageStruct() {
+	value := simplepb.SimpleMessage{
+		Id:          456,
+		IsPresent:   true,
+		MessageName: "message from proto",
+		List:        []int32{5, 7, 8},
+	}
+	fmt.Println(value)
+
 }
